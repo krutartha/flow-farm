e83d49cc62449ebe93a7c1b5f47e672c091aa36b
Krutartha <krutartha2002@gmail.com>
2021-09-19 10:53:51 -0400

    Initial out subtree commit gh-pages

---
+++ out/assets/technology.js
@@ -0,0 +1,12 @@
+export const technology = [
+  {
+    name: "NFT",
+    imgUrl: "/assets/nft_channels.jpg",
+    text: `This technology is based on supplying water directly to the roots of the plants, so that they can grow efficiently and easy because of the surplus amount of air available for the roots. NFT means Nutrient Film Technology which uses only the necessary nutrients required for the plants and it is supplied on the necessity of how the plants grow.`,
+  },
+  {
+    name: "Grow Bags",
+    imgUrl: "/assets/grow_bag_in_polyhouse.jpg",
+    text: "This technique allows us to control a single plant and have it grow into a wonderful vegetable bearing plant that has a very dynamic size and shape to it. Tasty Tomatoes, English Cucumber, Egg plant and any other tall plant can be grown using this technique.",
+  },
+];
