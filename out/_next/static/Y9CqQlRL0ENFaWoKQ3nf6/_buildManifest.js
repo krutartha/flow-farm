b46f61e71c11f12c9eac012be6be9610fd9a2539
Krutartha <krutartha2002@gmail.com>
2021-09-20 22:13:41 -0400

    Deploy gh-pages

--- out/_next/static/Y9CqQlRL0ENFaWoKQ3nf6/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/569-eeabc807fc2300ec98dd.js","static/css/93010fd490720bfb4297.css","static/chunks/pages/index-52449646d8bb708094cd.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
