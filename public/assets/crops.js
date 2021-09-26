d20f973170a2c295b3f62ae352a94ce7b7fe760b
Krutartha <krutartha2002@gmail.com>
2021-09-25 22:07:28 -0400

    added lettuce picture

--- public/assets/crops.js
+++ public/assets/crops.js
@@ -2,62 +2,62 @@ export const crops = [
   [
     {
       name: "Batavia",
-      imgUrl: "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png",
+      imgUrl: "/assets/lettuce.jpg",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
     },
     {
       name: "Kale",
-      imgUrl: "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png",
+      imgUrl: "/assets/lettuce.jpg",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
     },
     {
       name: "Butterhead",
-      imgUrl: "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png",
+      imgUrl: "/assets/lettuce.jpg",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
     },
     {
       name: "Lollo Rosa",
-      imgUrl: "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png",
+      imgUrl: "/assets/lettuce.jpg",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
     },
     {
       name: "Iceberg",
-      imgUrl: "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png",
+      imgUrl: "/assets/lettuce.jpg",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
     },
   ],
   [
     {
       name: "Bell Peppers",
-      imgUrl: "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png",
+      imgUrl: "/assets/lettuce.jpg",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
     },
   ],
   [
     {
       name: "English Cucumber",
-      imgUrl: "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png",
+      imgUrl: "/assets/lettuce.jpg",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
     },
   ],
   [
     {
       name: "Coriander",
-      imgUrl: "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png",
+      imgUrl: "/assets/lettuce.jpg",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
     },
   ],
   [
     {
       name: "Spinach",
-      imgUrl: "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png",
+      imgUrl: "/assets/lettuce.jpg",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
     },
   ],
   [
     {
       name: "Basil",
-      imgUrl: "https://www.firstbenefits.org/wp-content/uploads/2017/10/placeholder.png",
+      imgUrl: "/assets/lettuce.jpg",
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.",
     },
   ],
